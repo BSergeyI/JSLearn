6b9be348b013640b3b9a1591e70b646dfbe33591
Sergey <ya.sergeyonline@yandex.ru>
2019-07-10 19:55:57 +0300

    Добавлен index "шаблон" со скриптами wcommas, patt_form14.

---
+++ project12/js/patt_form14.js
@@ -0,0 +1,11 @@
+ function subValid() {
+        	let name_pat = /[a-z]/;
+        	let str_name = document.jsform.jsname.this.value;
+        	console.log(str_name);
+        	if (!name_pat.test(str_name)){
+        		console.log('error');
+        		this.border.color = "red";
+        		this.innerText = "Имя введено с ошибкой";
+        	}
+        }
+        	
